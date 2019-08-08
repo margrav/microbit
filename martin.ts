36ca0c383072bd7d1c22b25fa976023934d00149
margrav <martin.gravrak@bouvet.no>
2019-08-08 10:48:51 +0200

    Create martin.ts

---
+++ martin.ts
@@ -0,0 +1,6 @@
+function KastEkko (triggerPin: number, ekkoPin: number) {
+    pins.analogWritePin(AnalogPin.P0, triggerPin)
+}
+function SjekkAvstand (triggerPin: number, ekkoPin: number) {
+    pins.analogWritePin(AnalogPin.P0, triggerPin)
+}
